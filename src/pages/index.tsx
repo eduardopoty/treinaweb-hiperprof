ea70161b5db17809a3b1bad0fcc9bc96fb0ba7d3
eduardopoty <eduardo.poty@gmail.com>
2024-04-06 12:23:22 -0300

    Criando página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
 import PageTitle from "../ui/components/data-display/PageTitle";
+import { TextField, Icon, Button } from "@mui/material";
+import { HomeContainer, BoxButtons } from "@styles/pages/index.styles";
 
 export default function Home() {
   return (
-    <div>
-      <PageTitle title="TITULO" subtitle="Subtitulo Exemplo" />
-    </div>
-  )
+    <HomeContainer>
+      <PageTitle 
+        title="ENCONTRE O PROFESSOR IDEAL PARA VOCÊ!" 
+        subtitle="pesquise pelo professor ideal para você" 
+      />
+      <TextField 
+        sx={{ mt: 3, mb: 1}}
+        label={"Encontre um Professor"}
+        InputProps={{
+          startAdornment: <Icon>search</Icon>,
+        }}
+        fullWidth
+        required
+      />
+      <BoxButtons>
+        <Button type="submit" variant="contained">
+          Buscar o professor perfeito
+        </Button>
+      </BoxButtons>
+      
+    </HomeContainer>
+  );
 }
